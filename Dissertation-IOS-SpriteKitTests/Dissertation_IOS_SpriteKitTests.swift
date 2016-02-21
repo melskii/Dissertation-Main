34c899ed1c0f447e0503aab5bbe25039cf59bdcc
Melenie Schatynski <mschatynski@gmail.com>
2016-02-21 18:12:32 +0000

    Initial Commit

---
+++ Dissertation-IOS-SpriteKitTests/Dissertation_IOS_SpriteKitTests.swift
@@ -0,0 +1,36 @@
+//
+//  Dissertation_IOS_SpriteKitTests.swift
+//  Dissertation-IOS-SpriteKitTests
+//
+//  Created by Mel Schatynski on 21/02/2016.
+//  Copyright © 2016 Mel Schatynski. All rights reserved.
+//
+
+import XCTest
+@testable import Dissertation_IOS_SpriteKit
+
+class Dissertation_IOS_SpriteKitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
