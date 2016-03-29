d8ba538c330af4f24185eb24490cb2030c9e29c9
Melenie Schatynski <mschatynski@gmail.com>
2016-03-29 22:30:02 +0100

    Got the JSON into something I can read! WHOOOP!

--- Dissertation-IOS-SpriteKit/UserViewController.swift
+++ Dissertation-IOS-SpriteKit/UserViewController.swift
@@ -64,18 +64,47 @@ class UserViewController: UIViewController {
                 let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
                 print("RESPONSESTRING = \(responseString)")
             
-                let values: NSArray = try! NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as! NSArray
                 
+                var json: Array<AnyObject>!
                 
-                print("values \(values)")
-                
-                let userArray = values as! [String]
-                
-                if userArray.contains("id") {
-                    
-                    print("can I get a whoop whoop");
-                    
+                do {
+                    json = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions()) as? Array
+                } catch {
+                    print(error)
                 }
+                
+                
+                print(json)
+//               https://www.raywenderlich.com/120442/swift-json-tutorial
+                
+                guard let item = json[0] as? [String: AnyObject] ,
+                    
+                    let active = item["active"] as? String else {
+                        return;
+                    }
+                
+                print("ITEM \(item)")
+                print("active?: \(active)")
+                
+                
+//                let values: NSArray = try! NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as! NSArray
+//                
+//                print("data \(data)")
+                
+//                let jsonDic = try! NSJSONSerialization.JSONObjectWithData(values.firstObject as! NSData, options: NSJSONReadingOptions.MutableContainers) as! Dictionary<String, AnyObject>;
+                
+//                print("values \(values.firstObject)")
+                
+//                var dict = self.convertStringToDictionary(values.firstObject as! String)
+//                
+//                print("dict \(dict)")
+//            
+////
+////                if values.contains("id") {
+////                    
+////                    print("can I get a whoop whoop");
+////                    
+////                }
                 
                 
             }
@@ -86,6 +115,17 @@ class UserViewController: UIViewController {
         
 //        returnToPreviousController()
         
+    }
+    
+    func convertStringToDictionary(text: String) -> [String:AnyObject]? {
+        if let data = text.dataUsingEncoding(NSUTF8StringEncoding) {
+            do {
+                return try NSJSONSerialization.JSONObjectWithData(data, options: []) as? [String:AnyObject]
+            } catch let error as NSError {
+                print(error)
+            }
+        }
+        return nil
     }
     
   
